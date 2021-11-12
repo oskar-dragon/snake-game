664a07f3882dd23b1e156e0565d1a2600d654713
Oskar Dragon <oskartdragon@gmail.com>
2021-11-12 17:07:46 +0000

    Cleaned up code

--- index.pack.js
+++ index.pack.js
@@ -12,94 +12,99 @@ let speed = 0.9;
 let timerId = 0;
 
 function createGrid() {
-	for (let i = 0; i < width * width; i++) {
-		const newElement = document.createElement("div");
-		newElement.classList.add("grid-cell");
-		gridElement.append(newElement);
-		gridArray.push(newElement);
-	}
+  for (let i = 0; i < width * width; i++) {
+    const newElement = document.createElement("div");
+    newElement.classList.add("grid-cell");
+    gridElement.append(newElement);
+    gridArray.push(newElement);
+  }
 }
 
 createGrid();
 
-snakePosition.forEach((index) => gridArray[index].classList.add("snake"));
+snakePosition.forEach(index => gridArray[index].classList.add("snake"));
 
 function startRestartGame() {
-	snakePosition.forEach((index) => gridArray[index].classList.remove("snake"));
-	gridArray[appleIndex].classList.remove("apple");
+  snakePosition.forEach(index => gridArray[index].classList.remove("snake"));
+  gridArray[appleIndex].classList.remove("apple");
 
-	clearInterval(timerId);
-	snakePosition = [1, 2, 3];
-	direction = 1;
-	score = 0;
-	scoreElement.textContent = score;
-	time = 1000;
-	generateApples();
-	snakePosition.forEach((index) => gridArray[index].classList.add("snake"));
-
-	timerId = setInterval(move, time);
+  clearInterval(timerId);
+  snakePosition = [1, 2, 3];
+  direction = 1;
+  score = 0;
+  scoreElement.textContent = score;
+  time = 1000;
+  generateApples();
+  snakePosition.forEach(index => gridArray[index].classList.add("snake"));
+  timerId = setInterval(move, time);
 }
 
 function move() {
-	let head = snakePosition[snakePosition.length - 1];
-	// If statement checking if snake hits the wall or itself
-	if ((head + width >= width * width && direction === width) || (head % width === width - 1 && direction === 1) || (head % width === 0 && direction === -1) || (head - width < 0 && direction === -width) || gridArray[head + direction].classList.contains("snake")) {
-		return clearInterval(timerId);
-	}
+  let head = snakePosition[snakePosition.length - 1];
+  // If statement checking if snake hits the wall or itself
+  if (
+    (head + width >= width * width && direction === width) ||
+    (head % width === width - 1 && direction === 1) ||
+    (head % width === 0 && direction === -1) ||
+    (head - width < 0 && direction === -width) ||
+    gridArray[head + direction].classList.contains("snake")
+  ) {
+    return clearInterval(timerId);
+  }
 
-	let tail = snakePosition.shift();
-	gridArray[tail].classList.remove("snake");
-	let newHead = head + direction;
-	snakePosition.push(newHead);
+  let tail = snakePosition.shift();
+  gridArray[tail].classList.remove("snake");
+  let newHead = head + direction;
+  snakePosition.push(newHead);
 
-	if (gridArray[head].classList.contains("apple")) {
-		// remove the class of apple
-		gridArray[head].classList.remove("apple");
-		// grow our snake by adding class of snake to it
-		gridArray[tail].classList.add("snake");
-		// grow our snake array
-		snakePosition.unshift(tail);
-		//generate a new apple
-		generateApples();
-		//add one to the score
-		scoreElement.textContent++;
-		//speed up our snake
-		clearInterval(timerId);
-		time = time * speed;
-		timerId = setInterval(move, time);
-	}
+  if (gridArray[head].classList.contains("apple")) {
+    // remove the class of apple
+    gridArray[head].classList.remove("apple");
+    // grow our snake by adding class of snake to it
+    gridArray[tail].classList.add("snake");
+    // grow our snake array
+    snakePosition.unshift(tail);
+    //generate a new apple
+    generateApples();
+    //add one to the score
+    scoreElement.textContent++;
+    //speed up our snake
+    clearInterval(timerId);
+    time = time * speed;
+    timerId = setInterval(move, time);
+  }
 
-	gridArray[newHead].classList.add("snake");
+  gridArray[newHead].classList.add("snake");
 }
 
 function generateApples() {
-	do {
-		appleIndex = Math.floor(Math.random() * gridArray.length);
-	} while (gridArray[appleIndex].classList.contains("snake"));
-	gridArray[appleIndex].classList.add("apple");
+  do {
+    appleIndex = Math.floor(Math.random() * gridArray.length);
+  } while (gridArray[appleIndex].classList.contains("snake"));
+  gridArray[appleIndex].classList.add("apple");
 }
 
 generateApples();
 
 function control(event) {
-	switch (event.key) {
-		case "Right":
-		case "ArrowRight":
-			direction = 1;
-			break;
-		case "Up":
-		case "ArrowUp":
-			direction = -width;
-			break;
-		case "Left":
-		case "ArrowLeft":
-			direction = -1;
-			break;
-		case "Down":
-		case "ArrowDown":
-			direction = width;
-			break;
-	}
+  switch (event.key) {
+    case "Right":
+    case "ArrowRight":
+      direction = 1;
+      break;
+    case "Up":
+    case "ArrowUp":
+      direction = -width;
+      break;
+    case "Left":
+    case "ArrowLeft":
+      direction = -1;
+      break;
+    case "Down":
+    case "ArrowDown":
+      direction = width;
+      break;
+  }
 }
 
 // Event Listeners
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Snake Game
+
+> A simple Rock Paper Scissors game that everyone knows!
+> Live demo [_here_](https://oskar-dragon.github.io/snake-game/)
+
+## Table of Contents
+
+- [General Information](#general-information)
+- [Screenshots](#screenshots)
+- [Project Status](#project-status)
+- [Author Info](#author-info)
+
+## General Information
+
+Snake is a fun game to make as it doesn't require a lot of code.
+
+## Screenshots
+
+![Example screenshot](https://i.gyazo.com/358623a5dcfe9fd44bd971dd4337c685.png)
+
+## Project Status
+
+Project is: _complete_.
+
+## Author Info
+
+Created by [Oskar Dragon](https://github.com/oskar-dragon) |
+[LinkedIn](https://www.linkedin.com/in/oskar-dragon) |
+[Instagram](https://www.instagram.com/skrdrgn___/)
